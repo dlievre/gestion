46c17b0a6e91f9add5ec6ed1bef6add5afb4117d
dlievre <42@photeam.com>
2017-03-14 18:03:13 +0100

    tbl domaine

--- routes/index.js
+++ routes/index.js
@@ -13,41 +13,112 @@ app.use(bodyParser.urlencoded({extended: true})) // parsing application x/www-fo
 
 console.log('route index')
 
+var domaine = function() {
+//var domaine.vehicule = new Object()
+this.id = '',
+this.nom = '',
+this.menu = 'index',
+this.page = 'index',
+this.niv = '',
+this.msg = '',
+this.saisie = 'no'
+}
+
+var véhicule = new domaine()
+véhicule.niv = 'véhicule'
+véhicule.msg = 'Intervention véhicule'
+véhicule.saisie = 'yes'
+
+var voirie = new domaine()
+voirie.niv = 'voirie'
+voirie.msg = 'Intervention voirie'
+voirie.saisie = 'no'
+
+var voirie_encombrant = new domaine()
+voirie_encombrant.niv = 'voirie_encombrant'
+voirie_encombrant.msg = 'Intervention voirie / encombrant'
+voirie_encombrant.saisie = 'no'
+
+var voirie_mobilier = new domaine()
+voirie_mobilier.niv = 'voirie_mobilier'
+voirie_mobilier.msg = 'Intervention voirie / encombrant : mobilier'
+voirie_mobilier.saisie = 'yes'
+
+//véhicule.requete = 'request.body.'+véhicule.niv
+
+var domaines = new Object()
+domaines.véhicule = 'véhicule'
+domaines.voirie = 'voirie'
+domaines.voirie_encombrant = 'voirie_encombrant'
+domaines.voirie_mobilier = 'voirie_mobilier'
+
 
 app.post('/', upload.array(), (request, response) => {
 	var sess = request.session
 
+	for (variable in domaines) {	
+	requete = 'request.body.'+variable
+
 	var info = new Object()
+	if (eval(requete)){
+		console.log('req '+variable+ eval(variable.niv))
+			info.menu = eval(variable+'.menu')
+			info.page = eval(variable+'.page')
+			info.niv =  eval(variable+'.niv')
+			info.msg = eval(variable+'.msg')
+			info.saisie = eval(variable+'.saisie')
+
+	}
+	if (info.menu) response.render(info.page, { info })
+}
+
+	//var info = new Object()
 	info.titre_demande = "Dernières demandes"
 	info.page = 'index'
 	var menu = 'index'
 
-		if (request.body.voirie){
-			init = 'non'
-			info.menu = 'index'
-			info.page = 'index'
-			info.niv =  'voirie'
-			info.msg = 'Intervention voirie'
-			info.saisie = 'no'
+
+	// if (request.body.véhiculedd){
+		
+	// 		info.menu = véhicule.menu
+	// 		info.page = véhicule.page
+	// 		info.niv =  véhicule.niv
+	// 		info.msg = véhicule.msg
+	// 		info.saisie = véhicule.saisie
+
+	// 	//response.render(info.page, { info })
+
+	// }
+	
+
+	// 	if (request.body.voiriedd){
+	// 		console.log('voirie')
+	// 		console.log('post voirie '+request.body.voirie)
+	// 		init = 'non'
+	// 		info.menu = 'index'
+	// 		info.page = 'index'
+	// 		info.niv =  'voirie'
+	// 		info.msg = 'Intervention voirie'
+	// 		info.saisie = 'no'
 
 
-	}
-		if (request.body.voirie_encombrant){
-			info.menu = 'index'
-			info.page = 'index'
-			info.niv =  'voirie_encombrant'
-			info.msg = 'Intervention voirie'
-			info.saisie = 'no'
-	}
+	// }
+	// 	if (request.body.voirie_encombrant){
+	// 		info.menu = 'index'
+	// 		info.page = 'index'
+	// 		info.niv =  'voirie_encombrant'
+	// 		info.msg = 'Intervention voirie'
+	// 		info.saisie = 'no'
+	// }
 
-		if (request.body.voirie_mobilier){
-			info.menu = 'index'
-			info.page = 'index'
-			info.niv =  'voirie_mobilier'
-			info.msg = 'Intervention voirie'
-			info.saisie = 'yes'
-			//response.render('index', {menu, niv: "voirie_mobilier", msg:"Intervention voirie - Encombrant : Mobilier", saisie: 'ok', info })
-	}
+	// 	if (request.body.voirie_mobilier){
+	// 		info.menu = 'index'
+	// 		info.page = 'index'
+	// 		info.niv =  'voirie_mobilier'
+	// 		info.msg = 'Intervention voirie'
+	// 		info.saisie = 'yes'
+	// 		//response.render('index', {menu, niv: "voirie_mobilier", msg:"Intervention voirie - Encombrant : Mobilier", saisie: 'ok', info })
+	// }
 		if (request.body.accident){
 			info.menu = 'index'
 			info.page = 'index'
@@ -97,7 +168,7 @@ if (request.body.map){
 	
 }
 
-	if (info.menu != 'map') response.render(info.page, { info })
+	//if (info.menu != 'map') response.render(info.page, { info })
 
 })
 
