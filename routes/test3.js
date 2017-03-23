bc3dd98e6a044330deb8ddfda2abf16b364d59fb
dlievre <42@photeam.com>
2017-03-23 22:28:52 +0100

    Add files via upload

--- routes/test3.js
+++ routes/test3.js
@@ -1,11 +1,12 @@
 // ***** fichier test3.js
 var fs = require("fs")
- 
-_test3 = (request, response) => { // @ retourne la date formatée
-    var express = require('express')
+// ***** traitement super user
+_test3 = (request, response) => { 
+	// @ chargement des modules
+    var express = require('express') // @> express
     var app = express()
     //var router = express.Router();
-    var bodyParser = require('body-parser')// body-parser, formate le request d'un form
+    var bodyParser = require('body-parser')// @> body-parser, formate le request d'un form
     app.use(bodyParser.json()) // pour format json
     var multer = require('multer') // req.body récupère les champs d'un form
     var upload = multer()
@@ -29,13 +30,14 @@ var suivi = new Object()
 suivi.path = './routes'
 suivi.nbFilesTotal = 0
 suivi.FilesTraitment = 0
-
-debut0 = () =>{ // recupere les nom des fichiers d'un dossier
+//***** traitement de la documentation auto
+// @ recupere les nom des fichiers d'un dossier
+debut0 = () =>{ 
 	_filesDir(suivi.path, suite1)
 	console.log('debut0')
 }
-
-suite1 = (files)=>{ // ne prend que les .js
+// @ ne prend que les .js
+suite1 = (files)=>{ 
 	files.forEach( (file)=>{ 
 		if (file.indexOf('.js') >= 0 || file.indexOf('.ejs') >= 0){
 			++suivi.nbFilesTotal
@@ -44,52 +46,52 @@ suite1 = (files)=>{ // ne prend que les .js
 	console.log(' suivi.nbFilesTotal '+suivi.nbFilesTotal)
 	suite2(files)
 }
-
-suite2 = (files)=> { // lecture du contenu des fichiers
+// @ lecture du contenu des fichiers
+suite2 = (files)=> { 
+	var no = 0
 	files.forEach( (file)=>{
 		if (file.indexOf('.js') >= 0 || file.indexOf('.ejs') >= 0){
-		_readFile(suivi.path+'/'+file, file, suite3)
-			console.log('file : '+file)
+		_readFile(suivi.path+'/'+file, file, no++, suite3) // no permet de garder l'ordre
+			console.log(no+' file : '+file)
 		}
 	})
 }
-
-suite3 = (fichier, data)=>{ // concaténation du contenu
+// @ concaténation du contenu
+var results = []
+suite3 = (fichier, no, data)=>{ 
 	++suivi.FilesTraitment
 	console.log(suivi.FilesTraitment)
-	info.text += '***********************'+fichier+'\n'
-	info.text += data.toString()
-	console.log('fichier '+ fichier)
+	results[no] = '***** <b>'+fichier+'</b> *****\n'+data.toString()
+	console.log('no '+no+'fichier '+ fichier)
 	//console.log('data '+ data)
-	if (suivi.FilesTraitment === suivi.nbFilesTotal ) suite4(info.text)
+	if (suivi.FilesTraitment === suivi.nbFilesTotal ) {
+		for (i=0; i <= results.length; i++)
+			info.text += i+'  Fichier '+results[i]
+		console.log('fin concaténation ')
+		suite4(info.text)
+	}
 }
-
-suite4 = (data)=>{ // mise en forme du contenu
+// @ mise en forme du contenu
+suite4 = (data)=>{ 
 	console.log('decrypt')
-	console.log('tbl'+tbl.fichier)
 	decryptCode(data, suite5)
 	//suivi = ''
 }
-
-suite5 = (data)=>{ // affichage du résultat
-	info.text = data // on maj les données du tableau gal pour le passer ensuite
+// @ affichage du résultat
+suite5 = (data)=>{ 
+	info.text = data // on maj les données du tableau gal pour le passer ensuite au rendu
 	console.log('fin')
-	console.log('tbl'+tbl.fichier)
 	response.render(info.page, {info})
-	//suivi = ''
 }
 
 
 debut0()
 
-    //response.render(info.page, { info })
- 
-//    info.date = _fonctions.myDate()
 }
 
 
 
-// @ retourne la liste des fichiers d'un dossier
+// _filesDir @  retourne la liste des fichiers d'un dossier
 _filesDir = (dirname, callback)=> {
 	fs.readdir(dirname,(err, files)=>{
 	   if (err) { return console.error(err) }
@@ -97,22 +99,30 @@ _filesDir = (dirname, callback)=> {
 	})
 }
 
-_readFile = function(fichier, file, callback){ // @ lecture de fichier asynchrone
+// _readFile @ lecture de fichier asynchrone
+_readFile = function(fichier, file, no, callback){ 
 	fs.readFile(fichier, {encoding : 'utf8'}, (err, data) => {
-			console.log('data '+fichier)
-			callback(file, data)
+	var tblOut = "<br>"
+	//console.log(typeof (tblOut))
+	var tblIn = data.split("\n") // conversion en array avec \n comme delimiter
+	for (nb = 0; nb < tblIn.length; nb++)
+		tblOut += (nb+1)+' '+tblIn[nb]+'<br>'
+	console.log('data '+fichier)
+	callback(fichier, no, tblOut)
 	})
 }
 
-decryptCode = (data, callback)=>{ // @ filtre des lignes du code pour documentation automatique
+// decryptCode @  filtre des lignes du code pour documentation automatique
+decryptCode = (data, callback)=>{ 
 
-	var tblIn = data.split("\n") // conversion en array avec \n comme delimiter
+	var tblIn = data.split("<br>") // conversion en array avec \n comme delimiter
 	var tblOut = new Object()
 	var tblSyntax = new Object()
 	// @ liste des mots clé pris en compte
-	tblSyntax.a = 'require'
+	//tblSyntax.a = 'require'
 	tblSyntax.b = '@'
 	tblSyntax.c = '****'
+	tblSyntax.b = '#'
 	//tblSyntax.d = module.exports
 
 	console.log(typeof (data))
@@ -120,17 +130,24 @@ decryptCode = (data, callback)=>{ // @ filtre des lignes du code pour documentat
 	console.log('a'+tblSyntax)
 	console.log('b'+Object.values.tblSyntax) // ne fonctionne pas
 
-
 	for (nb = 0; nb < tblIn.length; nb++)
 	{
-		for (key in tblSyntax)
-			if (tblIn[nb].indexOf(tblSyntax[key]) >= 0) 
-				tblOut += (nb+1)+' '+tblIn[nb]+'<br>'
+		for (key in tblSyntax){
+			if (tblSyntax[key] == '#'){
+				console.log('finir traitement @>')
+				if (tblIn[nb].indexOf(tblSyntax[key]) >= 0) // traitement @>
+					tblOut += tblIn[nb].substr(tblIn[nb].indexOf(tblSyntax[key])) +'<br>'
+				if (tblIn[nb].indexOf(tblSyntax[key]) >= 0) // traitement @>
+					console.log(' n '+tblIn[nb].indexOf(tblSyntax[key]))
+				}
+			if (tblIn[nb].indexOf(tblSyntax[key]) >= 0) // ajouter @>
+				tblOut += tblIn[nb]+'<br>'
+		}
 	}
 
 	tblOut = tblOut.replace(/\[object Object\]/g, '')
 	tblOut = tblOut.replace(/var /g, '')
-	tblOut = tblOut.replace(/\//g, '')
+	tblOut = tblOut.replace(/\/\//g, '')
 	callback(tblOut)
 } 
  
